c013524db89ca19259eb12231136c76f501ba59a
Michael Bourke <git@elementality.com>
2014-05-31 14:53:19 -0400

    Remove native rebuild from AssetManager

--- assetmanager.py
+++ assetmanager.py
@@ -38,45 +38,22 @@ class AssetManager(object):
             return loader.load_bundles()
         return None
 
-    def _setup_environment(self, bundles, rebuild=True):
+    def _setup_environment(self, bundles):
         environment = webassets.Environment()
         environment.directory = self._resolve_assets_dir()
         for name, bundle in bundles.iteritems():
             environment.register(name, bundle)
-        environment.auto_build = rebuild
         return environment
 
-    def __init__(self, config_path, rebuild=False):
+    def __init__(self, config_path):
         bundles = self._load_asset_bundles(config_path)
-        self.environment = self._setup_environment(bundles, rebuild)
+        self.environment = self._setup_environment(bundles)
 
     def _resolve_assets_dir(self):
         for dirpath, dirnames, files in os.walk('.'):
             if 'assets' in dirnames:
                 return os.path.join(dirpath, 'assets')
         return None
-
-    def files_to_watch(self):
-        files, depends = [], []
-        ctx = wrap(self.environment, None)
-        for bundle in self.environment:
-            files.extend(bundle.resolve_contents(force=True))
-            depends.extend(bundle.resolve_depends(ctx))
-        orig, abspaths = zip(*files)
-        abspaths = list(abspaths)
-        abspaths.extend(depends)
-        return set(abspaths)
-
-    def _setup_rebuild(self):
-        pass
-        # watched = self._resolve_assets_dir()
-        # self.message("watching %s" % watched)
-        # rebuilder = FileModifiedHandler(self.build_environment, self.lock)
-        # observer = Observer()
-        # observer.schedule(rebuilder, watched, recursive=True)
-        # observer.daemon = True
-        # observer.start()
-        # observer.join()
 
     def build_environment(self):
         print "building environment..."
